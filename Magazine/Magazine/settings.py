61e2931da1798065c9fe2bcc5c0f0524da537100
Eldor <113456841+helloholop@users.noreply.github.com>
2023-06-23 18:51:04 +0500

    Update settings.py

--- Magazine/Magazine/settings.py
+++ Magazine/Magazine/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@)fun*45a6)c7hy88%heuana4q(vanvol%jcpflg^=jfbtbmqk'
+SECRET_KEY = '###'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -163,19 +163,19 @@ ACCOUNT_FORMS = {'signup': 'news.models.BasicSignupForm'}
 
 EMAIL_HOST = 'smtp.yandex.ru'  # адрес сервера Яндекс-почты для всех один и тот же
 EMAIL_PORT = 465  # порт smtp сервера тоже одинаковый
-EMAIL_HOST_USER = 'prodaldeda'  # ваше имя пользователя, например, если ваша почта user@yandex.ru, то сюда надо писать user, иными словами, это всё то что идёт до собаки
-EMAIL_HOST_PASSWORD = 'sgeecmskqyzwgpsh'  # пароль от почты
+EMAIL_HOST_USER = '###'  # ваше имя пользователя, например, если ваша почта user@yandex.ru, то сюда надо писать user, иными словами, это всё то что идёт до собаки
+EMAIL_HOST_PASSWORD = '###'  # пароль от почты
 EMAIL_USE_SSL = True
 
-DEFAULT_FROM_EMAIL='prodaldeda@yandex.ru'
+DEFAULT_FROM_EMAIL='###'
 
 
 APSCHEDULER_DATETIME_FORMAT = "N j, Y, f:s a"
 
 APSCHEDULER_RUN_NOW_TIMEOUT = 25
 
-CELERY_BROKER_URL ='redis://default:WLpxXGRLpf8D4kX0HbXlNS3PWVjCLYax@redis-11360.c62.us-east-1-4.ec2.cloud.redislabs.com:11360'
-CELERY_RESULT_BACKEND = 'redis://default:WLpxXGRLpf8D4kX0HbXlNS3PWVjCLYax@redis-11360.c62.us-east-1-4.ec2.cloud.redislabs.com:11360'
+CELERY_BROKER_URL ='##'
+CELERY_RESULT_BACKEND = '##'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
